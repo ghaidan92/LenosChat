9ca51d3e95eb728f61168be5e765de8d034d421b
Ghaidan Alhubaishi <alghaidan@yahoo.com>
2020-10-26 16:01:48 -0700

    added zoom-embed

--- server.js
+++ server.js
@@ -30,13 +30,6 @@ io.on('connection', socket => {
         .emit('message', formatMessage(botName,`${user.username} has joined the chat`));
     });
 
-    // Send users and room info
-    // io.to(user.room).emit('roomUsers', {
-    //     room: user.room,
-    //     users: getRoomUsers(user.room)
-
-    // })
-
     // Listen for chatMessage
     socket.on('chatMessage', (msg) => {
         const user = getCurrentUser(socket.id);
@@ -51,12 +44,6 @@ io.on('connection', socket => {
         if(user) {
             io.to(user.room).emit('message', formatMessage(botName,`${user.username} has left the chat`));
       };
-
-      // Send users and room info
-    //     io.to(user.room).emit('roomUsers', {
-    //     room: user.room,
-    //     users: getRoomUsers(user.room)
-    // }) 
 
     });
 })
